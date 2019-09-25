cb7080e2287064c07c31b828782640e121bc8e3c
Alvariytomyg <54126931+Alvariytomyg@users.noreply.github.com>
2019-09-24 21:36:01 -0500

    Subiendo

---
+++ ArtistfyPlatform/src/main/java/pe/edu/upc/entity/Person.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package pe.edu.upc.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author Alvaro
+ *
+ */
+@Entity
+@Table(name = "person")
+public class Person implements Serializable{
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
