e20cd9030e9c805adbf19693a2f85af4fefe9beb
programador <programador@VH203-27.upc.edu.pe>
2019-09-25 22:51:40 -0500

    agregue la clase person

--- ArtistfyPlatform/src/main/java/pe/edu/upc/entity/Person.java
+++ ArtistfyPlatform/src/main/java/pe/edu/upc/entity/Person.java
@@ -1,33 +1,5 @@
-/**
- * 
- */
 package pe.edu.upc.entity;
 
-import java.io.Serializable;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-/**
- * @author Alvaro
- *
- */
-@Entity
-@Table(name = "person")
-public class Person implements Serializable{
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String FirstName;
-    
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-    System.out.println("Aldf");
-	}
+public class Person {
 
 }
